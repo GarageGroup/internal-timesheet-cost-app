34a6093f214d43158b649ef69e04e92d2658844d
Егор Сухоруков <SEM200107@mail.ru>
2024-06-28 13:53:09 +0300

    fix test

--- src/endpoint/CostPeriod.GetSet/Test/Source.Func/Source.In.cs
+++ src/endpoint/CostPeriod.GetSet/Test/Source.Func/Source.In.cs
@@ -44,42 +44,6 @@ internal static partial class CostPeriodSetGetFuncSource
                             to: new(2024, 5, 15))
                     ]
                 }
-            },
-            {
-                new(
-                    value:
-                    [
-                        new PeriodJson()
-                        {
-                            Id = new("58ea8d90-a143-41c5-8bb3-02785def7011"),
-                            Name = "Some first name",
-                            From = new(2024, 6, 1, 23, 23, 1),
-                            To = new(2024, 6, 30, 23, 22, 3)
-                        },
-                        new PeriodJson()
-                        {
-                            Id= new("cd60a08b-22a0-4e32-869c-c0e6de7f4f11"),
-                            Name = "Some second name",
-                            From = new(2024, 5, 10, 23, 22, 3),
-                            To = new(2024, 5, 15, 23, 22, 3)
-                        }
-                    ]),
-                new()
-                {
-                    Periods =
-                    [
-                        new Timesheet.CostPeriod(
-                            id: new("58ea8d90-a143-41c5-8bb3-02785def7011"),
-                            name: "Some first name",
-                            from: new(2024, 6, 2),
-                            to: new(2024, 7, 1)),
-                        new Timesheet.CostPeriod(
-                            id: new("cd60a08b-22a0-4e32-869c-c0e6de7f4f11"),
-                            name: "Some second name",
-                            from: new(2024, 5, 11),
-                            to: new(2024, 5, 16))
-                    ]
-                }
             }
         };
 }
